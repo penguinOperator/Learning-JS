56777de669778b001c81b9d114f56d772db9317c
penguinOperator <penguingnop@gmail.com>
2025-07-03 11:37:49 +0800

    Added comments

--- js files/horroscope.js
+++ js files/horroscope.js
@@ -1,17 +1,19 @@
-let months = {
-    1: "May",
-    2: "June",
-    3: "April"
-};
+let months = [
+    "May",
+    "June",
+    "April"
+]; //add 3 months to months variable
 
 let horoscope = {
     "April": "Aries",
     "May": "Taurus",
     "June": "Gemini"
-};
+}; // assign the 3 months to zodiac signs
 
-let user = "May"
+let user = "May" // user chooses may, you can change this for a different response
 
-let sign = horoscope[user];
+let sign = horoscope[user];  // assign the value of the horoscope with the user to the sign variable
 
-console.log("Your zodiac sign is " + sign);
+console.log("Your zodiac sign is " + sign); //print with the sign variable
+
+//run with "node "horroscope.js"
