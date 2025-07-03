472b925c97bb41bf33ad6b345469f056410bee88
penguinOperator <penguingnop@gmail.com>
2025-07-04 01:05:41 +0800

    Improved code

--- js files/Blast Off.js
+++ js files/Blast Off.js
@@ -2,11 +2,7 @@ function countdown() {
     for (let i = 10; i >= 1; i--) {
         console.log(i)
     }
-    if (i = 1) {
-        console.log("BLAST OFF I GOT IT FINALLY")
-    } else {
-        console.log("Failed to blast off, we failed, well get em next time chat")
-    }
+            console.log("BLAST OFF")
 }
 
 countdown();
