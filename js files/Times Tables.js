dc16b30bcb71e087fe38384064f1ba1e2423187e
penguinOperator <penguingnop@gmail.com>
2025-07-03 12:35:48 +0800

    Practiced with arrays

---
+++ js files/Times Tables.js
@@ -0,0 +1,19 @@
+const timeTable = [
+    9 * 0,
+    9 * 1,
+    9 * 2,
+    9 * 3,
+    9 * 4,
+    9 * 5,
+    9 * 6,
+    9 * 7,
+    9 * 8,
+    9 * 9
+];
+
+let solve = 0;
+
+while (solve < timeTable.length) {
+    console.log(timeTable[solve]);
+    solve++;
+}
