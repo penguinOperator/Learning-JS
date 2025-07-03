ac40cd861ae6e09c7d5810d039e87baacf8235c0
penguinOperator <penguingnop@gmail.com>
2025-07-04 00:57:23 +0800

    Added countdown with text

---
+++ js files/Blast Off.js
@@ -0,0 +1,12 @@
+function countdown() {
+    for (let i = 10; i >= 1; i--) {
+        console.log(i)
+    }
+    if (i = 1) {
+        console.log("BLAST OFF I GOT IT FINALLY")
+    } else {
+        console.log("Failed to blast off, we failed, well get em next time chat")
+    }
+}
+
+countdown();
