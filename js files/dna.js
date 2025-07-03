e397601529768e0141cbe6f03a8d9aac603b5bed
penguinOperator <penguingnop@gmail.com>
2025-07-03 15:03:20 +0800

    fixed dna.js

---
+++ js files/dna.js
@@ -0,0 +1,13 @@
+const dnaPieces = ["A", "C", "G", "T"];
+const indexFinds = [];
+
+for (let i = 0; i < 24; i++) {
+    let indexFind = "";
+    for (let j = 0; j < 3; j++) {
+        let randomIndex = Math.floor(Math.random() * dnaPieces.length);
+        indexFind += dnaPieces[randomIndex];
+    }
+    indexFinds.push(indexFind);
+}
+
+console.log(indexFinds);
