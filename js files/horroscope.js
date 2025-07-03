53b952e8af390fa74197bee4c336eceb3044944e
penguinOperator <penguingnop@gmail.com>
2025-07-03 11:31:15 +0800

    removed comments

---
+++ js files/horroscope.js
@@ -0,0 +1,17 @@
+let months = {
+    1: "May",
+    2: "June",
+    3: "April"
+};
+
+let horoscope = {
+    "April": "Aries",
+    "May": "Taurus",
+    "June": "Gemini"
+};
+
+let user = "May"
+
+let sign = horoscope[user];
+
+console.log("Your zodiac sign is " + sign);
