37b7ec67b7d8bc474617d750c71c1f236f91b8bf
penguinOperator <penguingnop@gmail.com>
2025-07-14 18:37:17 +0800

    added code

---
+++ js files/reversewords.js
@@ -0,0 +1,15 @@
+const word = "racecar"
+const word1 = "madam"
+const word2 = "moonlight"
+const word3 = "aviary"
+
+function isPalindrome(word) {
+    let reversed = word.split('').reverse().join('');
+    if (word === reversed) {
+        console.log(word + " is a palindrome.");
+    } else {
+        console.log(word + " is not a palindrome.");
+    }
+}
+
+isPalindrome(word2)
