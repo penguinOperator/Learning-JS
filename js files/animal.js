6a4e4f93d01eb5835ebb1dab8422a5faa609668b
penguinOperator <penguingnop@gmail.com>
2025-07-15 10:50:03 +0800

    created files

---
+++ js files/animal.js
@@ -0,0 +1,30 @@
+animal1 = {
+    name: "hackwell",
+    age: 10,
+    type: "dog",
+    makeSounds () {
+        console.log("Woof woof im hackwell or call me bnard hackwell lol");
+    }
+}
+
+animal2 = {
+    name: "solar flare",
+    age: 3,
+    type: "cat",
+    makeSounds () {
+        console.log("meow im solar flare meow meow");
+    }
+}
+
+animal3 = {
+    name: "technoblade",
+    age: 10,
+    type: "pig",
+    makeSounds () {
+        console.log("Technoblade never dies!!!11");
+    }
+}
+
+animal1.makeSounds();
+animal2.makeSounds();
+animal3.makeSounds();
---
+++ js files/pokemon.js
@@ -0,0 +1,15 @@
+pokemon = {
+    name: "Pikachu",
+    type: "Electric ⚡️",
+    level: 25,
+    isCaught: false,
+}
+
+pokemon.isCaught = true;
+
+console.log("You caught " + pokemon.name + "! Rename him to anything.");
+
+pokemon.name = "Penguin"
+console.log("Name changed to " + pokemon.name);
+pokemon.type = "Water 💧"
+console.log("Type changed to " + pokemon.type);
